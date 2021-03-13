df39655cf5849b595d658215c8107c2e9cb7f6a8
Funnystyle <esero@hanmail.net>
2021-03-14 03:13:50 +0900

    #2.7 DOM - if else

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
+  const currentClass = title.className;
+  
+  if (currentClass !== CLICKED_CLASS) {
+    title.className = CLICKED_CLASS;
   } else {
-    title.style.color = BASE_COLOR;
+    title.className = "";
   }
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
 
 init();
-
-function handleOffline() {
-  console.log("Bye bye");
-}
-
-function handleOnline() {
-  console.log("Welcome back");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
