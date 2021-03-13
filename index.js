8351bd3a7aed7946b7e8828ad2afbcb2fc9b0077
Funnystyle <esero@hanmail.net>
2021-03-13 21:48:01 +0900

    #2.6 network event handler

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function init() {
 }
 
 init();
+
+function handleOffline() {
+  console.log("Bye bye");
+}
+
+function handleOnline() {
+  console.log("Welcome back");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
